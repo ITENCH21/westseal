3212d998a5f06755da358f733b068aedb3967fcb
Ivan <ivan@MacBook-Pro-Ivan.local>
2026-02-20 23:18:47 +0300

    feat: add EMAIL_USE_SSL support for mail.ru smtp

--- config/settings.py
+++ config/settings.py
@@ -145,6 +145,7 @@ EMAIL_HOST = os.environ.get("EMAIL_HOST", "")
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
-EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "1") == "1"
+EMAIL_USE_SSL = os.environ.get("EMAIL_USE_SSL", "0") == "1"
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "0") == "1"
 DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", "noreply@euro-seal.local")
 DEFAULT_TO_EMAIL = os.environ.get("DEFAULT_TO_EMAIL", "euro-seal@mail.ru")
